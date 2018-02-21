fa70750302e393c6098e97898a32ee06a1d2f934
alessandroaussems <alessandrossms@gmail.com>
2018-02-21 21:04:50 +0100

    BasicFonts+Colors

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}  - Chatbot</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('styling/main.css')}}">
+</head>
+<body>
+    <p>Karel</p>
+</body>
+</html>
