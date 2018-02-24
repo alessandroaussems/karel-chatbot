9cffba7f77d68b5200e6a6c863d325846002ce54
alessandroaussems <alessandrossms@gmail.com>
2018-02-24 18:25:10 +0100

    ConnectedWithWit.ai

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -10,5 +10,22 @@ class ChatController extends Controller
     {
         //ON ERROR: echo "ERROR";
         //RESPONSE: echo "Your response";
+        var_dump($this->CallWit($message));
+    }
+    function CallWit($message)
+    {
+        $witaccestoken=$_ENV['WITACCESSTOKEN'];
+        $url="https://api.wit.ai/message?q=".$message;
+        $authorization = "Authorization: Bearer ".$witaccestoken;
+        $ch=curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = json_decode(curl_exec($ch));
+        curl_close($ch);
+        return $result;
     }
 }
+
