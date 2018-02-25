8d16d5ea7a267a397e423f86213ced5616f5b9f0
alessandroaussems <alessandrossms@gmail.com>
2018-02-25 11:28:35 +0100

    MessagesAlignmentFix

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -17,8 +17,8 @@
 <main>
     <div class="chat">
         <ul id="messagelist">
-            <li class="botmessage">Bot</li>
-            <li class="usermessage">Me</li>
+            <li class="botmessage">Hallo! Ik ben Karel! Dé chatbot van KdG! Heb je vragen? Stel ze maar!</li>
+            <li class="usermessage">Hier komen berichten van de gebruiker!</li>
         </ul>
     </div>
     <input type="text" class="userinput" placeholder="Typ je bericht hier..." onkeydown="sendMessage(this)">
