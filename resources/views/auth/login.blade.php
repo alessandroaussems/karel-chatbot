f49f9ae647ae43b652a4f8cef5abbc0fe7c3dcfa
alessandroaussems <alessandrossms@gmail.com>
2018-02-27 21:19:26 +0100

    LoginBladeBasicStyling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,31 @@
 @extends('partials.header')
 
 @section('content')
+
                     <form method="POST" action="{{ route('login') }}" class="login">
                         @csrf
 
-                            <label for="email">E-Mail Address</label><br>
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong><br>
+                                    </span>
+                        @endif
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong><br>
+                                    </span>
+                        @endif 
+                            <label for="email">E-Mail:</label><br>
                             <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus><br>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
 
-                            <label for="password">Password</label><br>
+                            <label for="password">Paswoord:</label><br>
                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required><br>
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
 
 
-                            <div>
                                 <button type="submit">
                                     Login
                                 </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
                     </form>
 @endsection
