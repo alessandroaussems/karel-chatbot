dddb9947dad6354bd4bca0ed6ebea762b16882f7
alessandroaussems <alessandrossms@gmail.com>
2018-02-27 19:50:07 +0100

    BasicAuthentication

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/', function () {
     return view('chat');
 });
 Route::get('/chat/{message}','ChatController@handleMessage');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
