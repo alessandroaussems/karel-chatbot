eafd735f03859807af26bc7378bfa580a699eb2b
alessandroaussems <alessandrossms@gmail.com>
2018-03-03 14:17:26 +0100

    CreateNewAdmin(Register)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,12 +15,13 @@
                                         <strong>{{ $errors->first('password') }}</strong><br>
                                     </span>
                         @endif
+                        <br>
                             <label for="email">E-Mail:</label><br>
-                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus><br>
+                            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus><br>
 
 
                             <label for="password">Paswoord:</label><br>
-                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required><br>
+                            <input id="password" type="password"  name="password" required><br>
 
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,37 +1,37 @@
 @extends('partials.header')
 
 @section('content')
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}" class="register">
                         @csrf
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label><br>
-                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus><br>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback">
+                        @if ($errors->has('name'))
+                            <span class="invalid-feedback">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                @endif
-
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label><br>
-                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required><br>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
+                        @endif
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label><br>
-                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required><br>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
+                        @endif
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                        @endif
+                        <br>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Naam:</label><br>
+                            <input id="name" type="text"  name="name" value="{{ old('name') }}" required autofocus><br>
 
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label><br>
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required><br>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail:</label><br>
+                            <input id="email" type="email"  name="email" value="{{ old('email') }}" required><br>
+
+
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Paswoord:</label><br>
+                            <input id="password" type="password" name="password" required><br>
+
+
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Paswoord bevestigen:</label><br>
+                            <input id="password-confirm" type="password"  name="password_confirmation" required><br>
 
                                 <button type="submit">
                                     Register
