267de71da32f975859ed7a7eef1fffd28a195145
alessandroaussems <alessandrossms@gmail.com>
2018-03-03 15:02:50 +0100

    MigrationsFixed+MessagesTable

--- database/migrations/2018_03_03_135546_create_messages_table.php
+++ database/migrations/2018_03_03_135546_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        if(!Schema::hasTable('password_resets')) {
-            Schema::create('password_resets', function (Blueprint $table) {
-                $table->string('email')->index();
-                $table->string('token');
-                $table->timestamp('created_at')->nullable();
+        if(!Schema::hasTable('messages')) {
+            Schema::create('messages', function (Blueprint $table) {
+                $table->increments('id');
+                $table->longText("answer");
             });
         }
     }
@@ -29,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('messages');
     }
 }
