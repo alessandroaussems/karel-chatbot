66f825cfa8fb3fe2b22ef1edd2d40220d4f0ec2d
alessandroaussems <alessandrossms@gmail.com>
2018-03-17 16:04:30 +0100

    SessionSetup

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Session;
 
 class WelcomeController extends Controller
 {
@@ -13,6 +14,18 @@ class WelcomeController extends Controller
             $sessionid=uniqid();
             setcookie("chatsession", $sessionid);
             setcookie("visits",1);
+
+            $sessionmessages[0]="Hallo ik ben Karel! Stel je vragen maar!";
+            $sessionmessages[1]="Hallo ik ben Karel! Stel je vragen maar!";
+            $sessionmessages[2]="Hallo ik ben Karel! Stel je vragen maar!";
+            $sessionmessages[3]="Hallo ik ben Karel! Stel je vragen maar!";
+
+            $session = new Session();
+            $session->id = $sessionid;
+            $session->messages=json_encode($sessionmessages);
+            $session->lastactive=date('Y-m-d');
+            $session->save();
+
             return view("chat");
         }
         else
@@ -21,6 +34,9 @@ class WelcomeController extends Controller
             {
                 setcookie("visits",$_COOKIE["visits"]+=1);
             }
+            $session=Session::select('messages')->where('id', $_COOKIE["chatsession"])->first();
+            var_dump(json_decode($session->messages));
+
             return view("chat");
         }
     }
