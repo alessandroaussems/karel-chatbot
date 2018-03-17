c086faa18db3574bf9bea8c7c9d07c4467b7b0cb
alessandroaussems <alessandrossms@gmail.com>
2018-03-17 14:30:56 +0100

    AboutInChat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -6,6 +6,11 @@
             <li class="botmessage">Hallo! Ik ben Karel! Dé chatbot van KdG! Heb je vragen? Stel ze maar!</li>
             <li class="usermessage">Hier komen berichten van de gebruiker!</li>
         </ul>
+        <div class="opener">
+            <img src="{{asset("img/logo_black.png")}}" alt="Logo van Karel-Chatbot" title="Logo van Karel-Chatbot">
+            <h2>Karel - Chatbot</h2>
+            <p>Karel dé chatbot van KdG, voor al je vragen!</p>
+        </div>
     </div>
     <input type="text" class="userinput" placeholder="Typ je bericht hier..." onkeypress="sendMessage(this.value,event)" autofocus>
 <script src="{{asset('js/main.js')}}"></script>
