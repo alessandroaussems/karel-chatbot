e917537565136285bb98f78102374cc5da8d28b1
alessandroaussems <alessandrossms@gmail.com>
2018-03-24 21:30:28 +0100

    PasswordResetFixes

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -4,21 +4,21 @@
                         <form class="reset" method="POST" action="{{ route('password.request') }}">
                             {{ csrf_field() }}
                             @if ($errors->has('email'))
-                                <span class="help-block">
+                                <span class="invalid-feedback">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                             @endif
                             @if ($errors->has('password'))
-                                <span class="help-block">
+                                <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                             @endif
                             @if ($errors->has('password_confirmation'))
-                                <span class="help-block">
+                                <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
                                     </span>
                             @endif
-
+                            <br>
                             <input type="hidden" name="token" value="{{ $token }}">
                             <label for="email">E-Mail:</label><br>
                             <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus><br>
@@ -27,8 +27,8 @@
                             <label for="password">Wachtwoord:</label><br>
                             <input id="password" type="password"  name="password" required><br>
 
-                            <label for="password-confirm">Wachtwoord bevestigen:</label><br>
-                            <input id="password-confirm" type="password"  name="password-confirm" required><br>
+                            <label for="password_confirm">Wachtwoord bevestigen:</label><br>
+                            <input id="password-confirm" type="password"  name="password_confirmation" required><br>
 
                             <button type="submit">
                                 Wachtwoord herstellen
