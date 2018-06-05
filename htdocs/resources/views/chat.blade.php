ceb755271f0638c3d5430db1e6e691d263c517a9
alessandroaussems <alessandrossms@gmail.com>
2018-06-05 14:43:23 +0200

    Copy(Text)Edits+OptimizedMobileAppContent+AddedOptimizedOgImage

--- htdocs/resources/views/chat.blade.php
+++ htdocs/resources/views/chat.blade.php
@@ -12,13 +12,11 @@
                     <li class="usermessage"><?php echo $value[0] ?></li>
                 @endif
         @endforeach
-            <!--<li class="botmessage">Hallo! Ik ben Karel! Dé chatbot van KdG! Heb je vragen? Stel ze maar!</li>
-            <li class="usermessage">Hier komen berichten van de gebruiker!</li>-->
         </ul>
         <div class="opener">
             <img src="{{asset("img/logo_black.png")}}" alt="Logo van Karel-Chatbot" title="Logo van Karel-Chatbot">
             <h2>Karel - Chatbot</h2>
-            <p>Karel dé chatbot van KdG, voor al je vragen!</p>
+            <p>Dé chatbot van KdG</p>
         </div>
     </div>
     <input type="text" class="userinput" placeholder="Typ je bericht hier..." onkeypress="sendMessage(this.value,event)" autofocus>
--- htdocs/resources/views/partials/master.blade.php
+++ htdocs/resources/views/partials/master.blade.php
@@ -6,23 +6,21 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-title" content="Karel-Chatbot" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="white" />
+    <link rel="apple-touch-icon" href="{{asset('img/appicon.png')}}">
     <meta name="description" content="Karel-Chatbot" />
 
     <meta property="og:title" content="Karel-Chatbot" />
     <meta property="og:site_name" content="Karel-Chatbot" />
     <meta property="og:description" content="Dé chatbot van KdG" />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="{{asset('img/logo_white.png')}}" />
-
-    <meta property="twitter:card" content="summary" />
-    <meta property="twitter:title" content="Karel-Chatbot" />
-    <meta property="twitter:description" content="Dé chatbot van KdG" />
-    <meta property="twitter:image" content="{{asset('img/logo_white.png')}}" />
+    <meta property="og:image" content="{{asset('img/ogimage.png')}}" />
 
     <title>{{ config('app.name') }}</title>
+
     <link rel="icon" type="image/png" href="{{asset('img/logo_black.png')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('styling/main.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sceditor/2.1.3/themes/modern.min.css">
